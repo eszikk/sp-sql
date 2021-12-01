f38b29a0833a0bbf3454efdd544db6f61bbc9901
krisztian.eszik <eszik.k@gmail.com>
2021-12-01 16:54:01 +0100

    initial

--- src/schema.sql
+++ src/schema.sql
@@ -1,19 +1,45 @@
+-- ORGANIZATION
+create sequence ORGANIZATION_ID_SEQ
+/
+
+create table ORGANIZATION
+(
+    ID          NUMBER(38)                                               not null primary key,
+    NAME        VARCHAR2(255)                                            not null,
+    DESCRIPTION CLOB,
+    CREATED_AT  TIMESTAMP(6) WITH TIME ZONE default CURRENT_TIMESTAMP(6) not null,
+    UPDATED_AT  TIMESTAMP(6) WITH TIME ZONE,
+    DELETED_AT  TIMESTAMP(6) WITH TIME ZONE
+)
+/
+
+create or replace trigger ORGANIZATION_ID_TRG
+    before insert
+    on ORGANIZATION
+    for each row
+begin
+    if :new.id is null then
+        select ORGANIZATION_ID_SEQ.nextval into :new.id from dual;
+    end if;
+end;
+/
+
 -- PAGE
 create sequence PAGE_ID_SEQ
 /
 
 create table PAGE
 (
-    ID          NUMBER(38)                                               not null primary key,
-    NAME        VARCHAR2(255)                                            not null,
-    DESCRIPTION CLOB,
-    ACCESS_TYPE VARCHAR2(10)                default 'closed'             not null CHECK (ACCESS_TYPE IN ('open', 'closed') ),
-    AUDIENCE    VARCHAR2(10)                default 'internal'           not null CHECK (AUDIENCE IN ('internal', 'external') ),
-    PUBLISHED   NUMBER(1)                   default 0                    not null CHECK (PUBLISHED in (1, 0)),
-    CREATED_AT  TIMESTAMP(6) WITH TIME ZONE default CURRENT_TIMESTAMP(6) not null,
-    UPDATED_AT  TIMESTAMP(6) WITH TIME ZONE,
-    DELETED_AT  TIMESTAMP(6) WITH TIME ZONE
-    --PROJECT_ID  NUMBER(38)    not null references PROJECT
+    ID              NUMBER(38)                                               not null primary key,
+    NAME            VARCHAR2(255)                                            not null,
+    DESCRIPTION     CLOB,
+    ACCESS_TYPE     VARCHAR2(10)                default 'closed'             not null CHECK (ACCESS_TYPE IN ('open', 'closed') ),
+    AUDIENCE        VARCHAR2(10)                default 'internal'           not null CHECK (AUDIENCE IN ('internal', 'external') ),
+    PUBLISHED       NUMBER(1)                   default 0                    not null CHECK (PUBLISHED in (1, 0)),
+    CREATED_AT      TIMESTAMP(6) WITH TIME ZONE default CURRENT_TIMESTAMP(6) not null,
+    UPDATED_AT      TIMESTAMP(6) WITH TIME ZONE,
+    DELETED_AT      TIMESTAMP(6) WITH TIME ZONE,
+    ORGANIZATION_ID NUMBER(38)                                               not null references ORGANIZATION
 )
 /
 
@@ -85,22 +111,6 @@ begin
 end;
 /
 
--- COMPONENT_INCIDENT
-create table COMPONENT_INCIDENT
-(
-    COMPONENT_ID NUMBER(38) references COMPONENT not null,
-    INCIDENT_ID  NUMBER(38) references INCIDENT  not null
-)
-/
-
--- COMPONENT_SCHEDULED_MAINTENANCE
-create table COMPONENT_SCHEDULED_MAINTENANCE
-(
-    COMPONENT_ID             NUMBER(38) references COMPONENT             not null,
-    SCHEDULED_MAINTENANCE_ID NUMBER(38) references SCHEDULED_MAINTENANCE not null
-)
-/
-
 --INCIDENT
 create sequence INCIDENT_ID_SEQ
 /
@@ -157,3 +167,147 @@ begin
     end if;
 end;
 /
+
+-- COMPONENT_INCIDENT
+create table COMPONENT_INCIDENT
+(
+    COMPONENT_ID NUMBER(38) references COMPONENT not null,
+    INCIDENT_ID  NUMBER(38) references INCIDENT  not null,
+    CONSTRAINT COMPONENT_INCIDENT_UK unique (COMPONENT_ID, INCIDENT_ID)
+)
+/
+
+-- COMPONENT_SCHEDULED_MAINTENANCE
+create table COMPONENT_SCHEDULED_MAINTENANCE
+(
+    COMPONENT_ID             NUMBER(38) references COMPONENT             not null,
+    SCHEDULED_MAINTENANCE_ID NUMBER(38) references SCHEDULED_MAINTENANCE not null,
+    CONSTRAINT COMPONENT_SCHEDULED_MAINTENANCE_UK unique (COMPONENT_ID, SCHEDULED_MAINTENANCE_ID)
+)
+/
+
+-- TAG
+create sequence TAG_ID_SEQ
+/
+
+create table TAG
+(
+    ID         NUMBER(38)                                               not null primary key,
+    NAME       VARCHAR2(255)                                            not null,
+    SLUG       VARCHAR2(255)                                            not null,
+    CREATED_AT TIMESTAMP(6) WITH TIME ZONE default CURRENT_TIMESTAMP(6) not null,
+    UPDATED_AT TIMESTAMP(6) WITH TIME ZONE,
+    DELETED_AT TIMESTAMP(6) WITH TIME ZONE
+)
+/
+
+create or replace trigger TAG_ID_TRG
+    before insert
+    on TAG
+    for each row
+begin
+    if :new.id is null then
+        select TAG_ID_SEQ.nextval into :new.id from dual;
+    end if;
+end;
+/
+
+-- COMPONENT_TAG
+create table COMPONENT_TAG
+(
+    COMPONENT_ID NUMBER(38) references COMPONENT not null,
+    TAG_ID       NUMBER(38) references TAG       not null,
+    CONSTRAINT COMPONENT_TAG_UK unique (COMPONENT_ID, TAG_ID)
+)
+/
+
+-- SUBSCRIBER
+create sequence SUBSCRIBER_ID_SEQ
+/
+
+create table SUBSCRIBER
+(
+    ID          NUMBER(38)                                               not null primary key,
+    EMAIL       VARCHAR2(255)                                            not null,
+    VERIFY_CODE VARCHAR2(255)                                            not null,
+    CREATED_AT  TIMESTAMP(6) WITH TIME ZONE default CURRENT_TIMESTAMP(6) not null,
+    UPDATED_AT  TIMESTAMP(6) WITH TIME ZONE,
+    DELETED_AT  TIMESTAMP(6) WITH TIME ZONE
+)
+/
+
+create or replace trigger SUBSCRIBER_ID_TRG
+    before insert
+    on SUBSCRIBER
+    for each row
+begin
+    if :new.id is null then
+        select SUBSCRIBER_ID_SEQ.nextval into :new.id from dual;
+    end if;
+end;
+/
+
+-- COMPONENT_SUBSCRIBER
+create table COMPONENT_SUBSCRIBER
+(
+    COMPONENT_ID  NUMBER(38) references COMPONENT  not null,
+    SUBSCRIBER_ID NUMBER(38) references SUBSCRIBER not null,
+    CONSTRAINT COMPONENT_SUBSCRIBER_UK unique (COMPONENT_ID, SUBSCRIBER_ID)
+)
+/
+
+-- USER
+create sequence APP_USER_ID_SEQ
+/
+
+create table APP_USER
+(
+    ID             NUMBER(38)                                               not null primary key,
+    USERNAME       VARCHAR2(255)                                            not null
+        constraint USER_USERNAME_UK unique,
+    "PASSWORD"     VARCHAR2(255)                                            not null,
+    REMEMBER_TOKEN VARCHAR2(100),
+    EMAIL          VARCHAR2(255)                                            not null
+        constraint USER_EMAIL_UK unique,
+    API_KEY        VARCHAR2(255)                                            not null
+        constraint USER_API_KEY_UK unique,
+    ACTIVE         NUMBER(1)                   default 0                    not null CHECK (ACTIVE in (1, 0)),
+    CREATED_AT     TIMESTAMP(6) WITH TIME ZONE default CURRENT_TIMESTAMP(6) not null,
+    UPDATED_AT     TIMESTAMP(6) WITH TIME ZONE,
+    DELETED_AT     TIMESTAMP(6) WITH TIME ZONE
+)
+/
+
+create or replace trigger APP_USER_ID_TRG
+    before insert
+    on APP_USER
+    for each row
+begin
+    if :new.id is null then
+        select APP_USER_ID_SEQ.nextval into :new.id from dual;
+    end if;
+end;
+/
+
+-- ENTITLEMENT
+create table ENTITLEMENT
+(
+    ID         NUMBER(38)                                               not null primary key,
+    NAME       VARCHAR2(255)                                            not null
+        constraint ENTITLEMENT_NAME_UK unique,
+    CREATED_AT TIMESTAMP(6) WITH TIME ZONE default CURRENT_TIMESTAMP(6) not null,
+    UPDATED_AT TIMESTAMP(6) WITH TIME ZONE,
+    DELETED_AT TIMESTAMP(6) WITH TIME ZONE
+)
+/
+
+-- USER_ENTITLEMENT
+create table APP_USER_ENTITLEMENT
+(
+    USER_ID        NUMBER(38) references APP_USER    not null,
+    ENTITLEMENT_ID NUMBER(38) references ENTITLEMENT not null,
+    CONSTRAINT APP_USER_ENTITLEMENT_UK unique (USER_ID, ENTITLEMENT_ID)
+)
+/
+
+
